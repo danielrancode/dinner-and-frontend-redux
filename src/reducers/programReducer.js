aa24fba10e2aa4a83db61e7fba66e9b79669e7e0
Daniel Ran <danielr1735@gmail.com>
2018-09-22 20:00:32 -0400

    refactor reducers, BUGGY

---
+++ src/reducers/programReducer.js
@@ -0,0 +1,37 @@
+const initialProgramState = {
+  programs: [],
+  currentProgram: null,
+  currentEvent: null,
+  currentRestaurant: null,
+}
+
+export const program = (state = initialProgramState, action) => {
+  switch(action.type) {
+    case 'SELECT_EVENT':
+      if (state.currentEvent === action.payload) {
+        return { ...state, currentEvent: null }
+      } else {
+        return { ...state, currentEvent: action.payload }
+      }
+    case 'SELECT_RESTAURANT':
+      if (state.currentRestaurant === action.payload) {
+        return { ...state, currentRestaurant: null }
+      } else {
+        return { ...state, currentRestaurant: action.payload }
+      }
+    case 'SELECT_PROGRAM':
+      return { ...state, currentProgram: action.program}
+    case 'CREATE_PROGRAM':
+      return state
+    case 'EDIT_PROGRAM':
+      return state
+    case 'UPDATE_PROGRAM':
+      return state
+    case 'DELETE_PROGRAM':
+      return state
+    default:
+      return state;
+  }
+}
+
+export default program
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,32 @@
+let currentUser = {name: 'daniel', password: "hello!", id: 100}
+
+const initialUserState = currentUser ? { loggedIn: true, currentUser: user } : { loggedIn: false }
+
+const user = (state = initialUserState, action) => {
+  switch(action.type) {
+    case 'CREATE_USER':
+      return state
+    case 'LOGIN_REQUEST':
+      return {
+        loggingIn: true,
+        user: action.user
+      }
+    case 'LOGIN_SUCCESS':
+      return {
+        loggedIn: true,
+        user: action.user
+      }
+    case 'LOGIN_FAILURE':
+      return {
+        loggedIn: false
+      }
+    case 'LOGOUT':
+      return {
+        loggedIn: false
+      }
+    default:
+      return state;
+  }
+}
+
+export default user

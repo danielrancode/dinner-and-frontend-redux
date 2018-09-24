bdefdcfeac77f50ae03c69bc0b1f37d6ac985849
Daniel Ran <danielr1735@gmail.com>
2018-09-24 13:10:41 -0400

    added login form

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const LogInForm = () => {
-  return (
-    <h1>LogInForm!</h1>
-  )
+
+class LogInForm extends Component {
+
+  state = { username: '', password: '' }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <label>username:<input type='text' name='username' onChange={this.handleChange.bind(this)}/></label>
+        <label>password:<input type='password' name='password' onChange={this.handleChange.bind(this)}/></label>
+        <button type='submit'>Submit</button>
+      </form>
+    )
+  }
 }
 
 export default LogInForm

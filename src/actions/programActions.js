16b017ce686ea2d78d381f2b79b56b2fde2639d9
Daniel Ran <danielr1735@gmail.com>
2019-01-17 14:45:09 -0500

    fixed jwt bug

--- src/actions/programActions.js
+++ src/actions/programActions.js
@@ -2,14 +2,13 @@ import * as types from '../types.js'
 
 const PROGRAMS_URL = userId => `${process.env.REACT_APP_API_ENDPOINT}/api/v1/users/${userId}/programs`
 const PROGRAM_URL = program => `${process.env.REACT_APP_API_ENDPOINT}/api/v1/users/${program.user_id}/programs/${program.id}`
-const TOKEN = localStorage.getItem('jwt')
 
 export const fetchPrograms = (userId) => {
   return (dispatch) => {
     dispatch({ type: types.START_ADDING_PROGRAMS_REQUEST })
     fetch(PROGRAMS_URL(userId), {
       method: 'GET',
-      headers: { Authorization: `Bearer ${TOKEN}` }
+      headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` }
     })
       .then(res => res.json())
       .then(data => {
@@ -23,7 +22,7 @@ export const createProgram = (userId, data) => {
     dispatch({ type: types.START_SAVING_PROGRAM_REQUEST })
     fetch(PROGRAMS_URL(userId), {
         method: 'POST',
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${TOKEN}` },
+        headers: { "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem('jwt')}` },
         body: JSON.stringify({data: data})
       })
     .then(res => {
@@ -37,7 +36,7 @@ export const deleteProgram = (program) => {
     dispatch({ type: types.START_DELETING_PROGRAM_REQUEST })
     fetch(PROGRAM_URL(program), {
         method: 'DELETE',
-        headers: { Authorization: `Bearer ${TOKEN}` },
+        headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` },
       })
     .then(res => {
       if (res.ok) { dispatch({ type: types.DELETE_SUCCESS }) } else { throw res }

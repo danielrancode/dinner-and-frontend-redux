9ebf3dd71a86c343257dfd4cd97c81e08e770e26
Daniel Ran <danielr1735@gmail.com>
2019-01-31 19:25:02 -0500

    improved MyPrograms appearance

--- src/components/Program.js
+++ src/components/Program.js
@@ -3,24 +3,27 @@ import { connect } from  'react-redux'
 import { deleteProgram, fetchPrograms } from '../actions'
 import Restaurant from './Restaurant'
 import Event from './Event'
+import '../assets/css/Program.css'
+import ampersand from '../assets/logoAmp.png'
+import deleteIcon from '../assets/delete-icon.png'
+
+
 
 const Program = ({ program, deleteProgram }) => {
   let restaurant = JSON.parse(program.restaurant.json_data)
   let event = JSON.parse(program.event.json_data)
 
   return (
-    <Fragment>
-      <h1>Program {program.id}</h1>
-      <h3>Restaurant:</h3>
-      <ul>
+    <div className="program">
         <Restaurant key={restaurant ? restaurant.id : null} restaurant={restaurant}/>
-      </ul>
-      <h3>Event:</h3>
-      <ul>
+        <div id="ampersand">
+          <img src={ampersand}/>
+        </div>
         <Event key={event ? event.id : null} event={event} />
-      </ul>
-      <button onClick={() => deleteProgram(program)}>Delete</button>
-    </Fragment>
+        <div id="delete">
+          <button onClick={() => deleteProgram(program)}><img src={deleteIcon}/></button>
+        </div>
+    </div>
   )
 }
 
